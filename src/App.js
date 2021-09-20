6238c5cae9eb18a7092886aa96e759a9a0c52594
Viviane Ribeiro <vivy.saribeiro@gmail.com>
2021-09-19 23:02:10 -0300

    fix tab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css'
 import BackgroundSection from './components/background-section/background-section.js'
-import TabSection from './components/tab-section/tab-section.js'
-import CardSection from './components/cards-sections/cards-section-tecnologys'
+import Cards from './components/tab-section/tab-section.js'
+// import {portifolio} from '../data'
+import { useState } from 'react'
 
 function App() {
+  const [indice, setIndice] = useState(0)
   return (
     <>
       <BackgroundSection/>
-      <TabSection/>
-      {/* <CardSection/> */}
+      <section className="section-tabs">
+        <div className="tab-list">
+          <div className="scroll">
+            <button className="tab-btn" onClick={()=> setIndice(0)} className= {indice === 0 ? " active" : "tab-btn" }>👩‍💻 Tecnologias</button>
+            <button className="tab-btn" onClick={()=> setIndice(1)} className= {indice === 1 ? " active" : "tab-btn" }>🧘🏼‍♀️ Hobbies</button>
+            <button className="tab-btn" onClick={()=> setIndice(2)} className= {indice === 2 ? " active" : "tab-btn" }>🎥 Top Filmes</button>
+          </div>
+        </div>
+      </section>
+      <Cards indice={indice}/>
     </>
   );
 }
